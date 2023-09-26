e5632cb7c341a13ad514a29c8f3b5e39e4990e02
smokestacklightnin <125844868+smokestacklightnin@users.noreply.github.com>
2023-09-25 21:54:37 -0700

    Implement dfs

--- src/unit_table.cpp
+++ src/unit_table.cpp
@@ -1,14 +1,51 @@
+#include <stack>
 #include "unit_table.hpp"
-
-void unittable::unittable::add_unit(std::string from_id, std::string to_id, float factor) {
+#include <iostream>
+void unittable::unittable::add_unit(const std::string &from_id, const std::string &to_id, float factor) {
   this->m_table[from_id][to_id] = factor;
   this->m_table[to_id][from_id] = 1. / factor;
 }
 
-float unittable::unittable::get_converstion(std::string from_id, std::string to_id, const float &to_convert) {
+float unittable::unittable::get_converstion(const std::string &from_id, const std::string &to_id, const float &to_convert) {
   return to_convert * this->get_unit_conversion(from_id, to_id);
 }
 
-float unittable::unittable::get_unit_conversion(std::string from_id, std::string to_id) {
-  return 0;
+float unittable::unittable::get_unit_conversion(const std::string &from_id, const std::string &to_id) {
+  std::unordered_map <  std::string, std::pair< float,  std::string >> m_visited;   // value and parent
+  m_visited.clear();
+  std::stack<std::string> m_stack;
+  std::string prev_unit(from_id), curr_unit("");
+  m_stack.push(from_id);
+  float output = -1.;
+
+  while (!m_stack.empty()) {
+    curr_unit = m_stack.top();
+    m_stack.pop();
+    m_visited.insert(std::pair<std::string, std::pair<float, std::string>>(curr_unit, std::pair<float, std::string>(this->m_table[prev_unit][curr_unit], prev_unit)));
+    prev_unit = curr_unit;
+
+    for (auto it = (this->m_table)[curr_unit].begin(); it != (this->m_table)[curr_unit].end(); it++) {
+      if (m_visited.find(it->first) == m_visited.end()) {
+        m_stack.push(it->first);
+      }
+    }
+
+    if (curr_unit == to_id) {
+      output = 0; // success
+      break;
+    }
+  }
+
+  if (output == 0) {
+    prev_unit = m_visited[curr_unit].second;
+    output = 1.0;
+
+    while (curr_unit != from_id) {
+      output *=  this->m_table[prev_unit][curr_unit];
+      curr_unit = prev_unit;
+      prev_unit = m_visited[prev_unit].second;
+    }
+  }
+
+  return output;
 }
--- src/unit_table.hpp
+++ src/unit_table.hpp
@@ -1,18 +1,16 @@
 #ifndef UNIT_TABLE
 #define UNIT_TABLE
 #include <string>
+#include <map>
 #include <unordered_map>
 namespace unittable {
 class unittable {
  public:
-  float get_converstion(std::string from_id, std::string to_id, const float &to_convert);
-  float get_unit_conversion(std::string from_id, std::string to_id);
-  void add_unit(std::string from_id, std::string to_id, float factor);
+  float get_converstion(const std::string &from_id, const std::string &to_id, const float &to_convert);
+  float get_unit_conversion(const std::string &from_id, const std::string &to_id);
+  void add_unit(const std::string &from_id, const std::string &to_id, float factor);
  private:
-  bool dfs();
-  bool dfs_util(std::string id);
-  std::unordered_map<std::string, std::unordered_map<std::string, float>> m_table;
-  std::unordered_map < std::string, float> m_visited;
+  std::unordered_map< std::string, std::map< std::string,  float >> m_table;
 };
 }
 #endif
