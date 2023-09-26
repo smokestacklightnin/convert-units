2687c0316dfc8bcacd8af7279e44ef7a95a712f4
smokestacklightnin <125844868+smokestacklightnin@users.noreply.github.com>
2023-09-25 21:55:28 -0700

    Add a basic test

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,13 @@
 #include <iostream>
-
+#include "unit_table.hpp"
 int main() {
   std::cout << "Units" << std::endl;
+  unittable::unittable U;
+  U.add_unit("ft", "in", 12);
+  U.add_unit("mi", "ft", 5280);
+  U.add_unit("in", "cm", 2.54);
+  U.add_unit("m", "cm", 100);
+  U.add_unit("km", "m", 1000);
+  std::cout << U.get_unit_conversion("mi", "km") << '\t' << 1.60934 << std::endl;
   return 0;
 }
