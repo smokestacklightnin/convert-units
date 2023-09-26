692da4a31bb08708d90b28dbb8a5192136a595cb
smokestacklightnin <125844868+smokestacklightnin@users.noreply.github.com>
2023-09-25 18:56:48 -0700

    Add header for `unittable`

---
+++ src/unit_table.hpp
@@ -0,0 +1,18 @@
+#ifndef UN0_TABLE
+#define
+#include <string>
+#include <unordered_map>
+namespace unittable {
+class unittable {
+ public:
+  float get_converstion(const float &to_convert);
+  float get_unit_conversion(const float &to_convert);
+  void add_unit(std::string from_id, std::string to_id);
+ private:
+  bool dfs();
+  bool dfs_util(std::string id);
+  unordered_map<std::string, unordered_map<std::string, float>> m_table;
+  unordered_map < std::string, float> m_visited;
+}
+}
+#endif
